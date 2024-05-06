b2619f36d857a6f6e77483cc4baf823d7886dd8c
victoriasv07 <victoria2007silva@gmail.com>
2024-05-06 07:48:52 -0300

    v0.1

---
+++ tempo_viagem.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distância: "))
+velocidade = float(input("Digite a velocidade: "))
+tempo = distancia/velocidade
+
+print(f"Você irá chegar no tempo de: {tempo}hrs ")
