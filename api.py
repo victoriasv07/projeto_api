f60e303b54bfce1b003d3fa7c289daf3e79c2350
victoriasv07 <victoria2007silva@gmail.com>
2024-05-07 09:04:12 -0300

    criando api

---
+++ api.py
@@ -0,0 +1,52 @@
+from flask import Blueprint
+from flask import Flask, jsonify
+from random_data import pessoas
+import  funcoes
+
+
+bp = Blueprint("api", __name__)
+
+app = Flask(__name__)
+@bp.route('/')
+def index():
+    return jsonify({"status": 200, "message": "API do VICTORIA_SILVA_VIEIRA"})
+
+@bp.route("/aleatorios")
+def aleatorios():
+    import random
+    a = random.randint(49,100)
+    return jsonify({"status": 200, "data": a})
+
+
+@bp.route('/argumentos/<string:nome>')
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
+
+
+@bp.route("/maior_de_50", methods=["GET"])
+def maior_de_50():
+    count = funcoes.maior_de_50(pessoas)
+    return jsonify({"status": 200, "Pessoas com mais de 50 anos:": count})
+
+@bp.route("/mais_2000", methods=["GET"])
+def mais_2000():
+    count, x, total_registros = funcoes.mais_2000(pessoas)
+    return jsonify({"status": 200, "Pessoas com registros": total_registros, "Pessoas que ganham mais de 2000:": count, "Porcentagem de pessoas que ganham 2000:": x})
+
+@bp.route("/media_profissoes", methods=["GET"])
+def media_profissoes():
+    profissoes = funcoes.media_profissoes(pessoas)
+    return jsonify({"status": 200, "Descrição das profissões:": profissoes})
+
+@bp.route("/tres_maiores", methods=["GET"])
+def tres_maiores():
+    tres_maiores = funcoes.tres_salarios(pessoas)
+    return jsonify({"status": 200, "Os três maiores salários": tres_maiores})
+
+@bp.route("/sexo_idades", methods=["GET"])
+def sexo_idades():
+    count_feminino, count_masculino, intervalo_idades, sexo_maior_2000 = funcoes.sexo_idade(pessoas)
+    return jsonify({"status": 200, "Mulheres que ganham mais de 2000": count_feminino, "Homens que ganham mais de 2000": count_masculino, "intervalo da idade de quem ganha mais de 2000": intervalo_idades, "Sexo que há mais pessoas que ganham mais de 2000": sexo_maior_2000})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app.py
+++ app.py
@@ -2,47 +2,49 @@ from flask import Flask, jsonify
 from random_data import pessoas
 import  funcoes
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return jsonify({"status": 200, "message": "API do VICTORIA_SILVA_VIEIRA"})
-
-@app.route("/aleatorios")
-def aleatorios():
-    import random
-    a = random.randint(49,100)
-    return jsonify({"status": 200, "data": a})
 
 
-@app.route('/argumentos/<string:nome>')
-def argumentos(nome: str):
-    return jsonify({"status": 200, "data": nome})
+# app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     return jsonify({"status": 200, "message": "API do VICTORIA_SILVA_VIEIRA"})
+
+# @app.route("/aleatorios")
+# def aleatorios():
+#     import random
+#     a = random.randint(49,100)
+#     return jsonify({"status": 200, "data": a})
 
 
-@app.route("/maior_de_50", methods=["GET"])
-def maior_de_50():
-    count = funcoes.maior_de_50(pessoas)
-    return jsonify({"status": 200, "Pessoas com mais de 50 anos:": count})
+# @app.route('/argumentos/<string:nome>')
+# def argumentos(nome: str):
+#     return jsonify({"status": 200, "data": nome})
 
-@app.route("/mais_2000", methods=["GET"])
-def mais_2000():
-    count, x, total_registros = funcoes.mais_2000(pessoas)
-    return jsonify({"status": 200, "Pessoas com registros": total_registros, "Pessoas que ganham mais de 2000:": count, "Porcentagem de pessoas que ganham 2000:": x})
 
-@app.route("/media_profissoes", methods=["GET"])
-def media_profissoes():
-    profissoes = funcoes.media_profissoes(pessoas)
-    return jsonify({"status": 200, "Descrição das profissões:": profissoes})
+# @app.route("/maior_de_50", methods=["GET"])
+# def maior_de_50():
+#     count = funcoes.maior_de_50(pessoas)
+#     return jsonify({"status": 200, "Pessoas com mais de 50 anos:": count})
 
-@app.route("/tres_maiores", methods=["GET"])
-def tres_maiores():
-    tres_maiores = funcoes.tres_salarios(pessoas)
-    return jsonify({"status": 200, "Os três maiores salários": tres_maiores})
+# @app.route("/mais_2000", methods=["GET"])
+# def mais_2000():
+#     count, x, total_registros = funcoes.mais_2000(pessoas)
+#     return jsonify({"status": 200, "Pessoas com registros": total_registros, "Pessoas que ganham mais de 2000:": count, "Porcentagem de pessoas que ganham 2000:": x})
 
-@app.route("/sexo_idades", methods=["GET"])
-def sexo_idades():
-    count_feminino, count_masculino, intervalo_idades, sexo_maior_2000 = funcoes.sexo_idade(pessoas)
-    return jsonify({"status": 200, "Mulheres que ganham mais de 2000": count_feminino, "Homens que ganham mais de 2000": count_masculino, "intervalo da idade de quem ganha mais de 2000": intervalo_idades, "Sexo que há mais pessoas que ganham mais de 2000": sexo_maior_2000})
+# @app.route("/media_profissoes", methods=["GET"])
+# def media_profissoes():
+#     profissoes = funcoes.media_profissoes(pessoas)
+#     return jsonify({"status": 200, "Descrição das profissões:": profissoes})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route("/tres_maiores", methods=["GET"])
+# def tres_maiores():
+#     tres_maiores = funcoes.tres_salarios(pessoas)
+#     return jsonify({"status": 200, "Os três maiores salários": tres_maiores})
+
+# @app.route("/sexo_idades", methods=["GET"])
+# def sexo_idades():
+#     count_feminino, count_masculino, intervalo_idades, sexo_maior_2000 = funcoes.sexo_idade(pessoas)
+#     return jsonify({"status": 200, "Mulheres que ganham mais de 2000": count_feminino, "Homens que ganham mais de 2000": count_masculino, "intervalo da idade de quem ganha mais de 2000": intervalo_idades, "Sexo que há mais pessoas que ganham mais de 2000": sexo_maior_2000})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
